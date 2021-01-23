a48a0d5fde71a25d9380b8d8892de03d1ebc5cf7
Anantadinath <vickey.coool@gmail.com>
2021-01-23 13:38:50 +0530

    updated pipe

--- Rscripts/testing_nlp.R
+++ Rscripts/testing_nlp.R
@@ -21,20 +21,19 @@ year <- year(reg_date)
 month <- month(reg_date)
 mday <- data.table::mday(reg_date)
 
-
 url <- sprintf(
   "https://www.dailyfx.com/forex/market_alert/%02d/%02d/%02d/Risk-Events-for-January-Why-the-Georgia-Senate-Runoff-is-Key-for-Financial-Markets.html",
   year,
   month,
-  mday
+  4
 )
 
-news_text <- read_html(url) %>% 
-  html_node(css = ".dfx-articleBody .dfx-articleBody__content") %>% 
+news_text <- read_html(url) |> 
+  html_node(css = ".dfx-articleBody .dfx-articleBody__content") |> 
   html_text()
 
-sec_token <- news_text %>% 
-  corpus() %>% 
+sec_token <- news_text |>
+  corpus() |>
   tokens(
     what = "word",
     remove_punct = TRUE,
@@ -46,37 +45,40 @@ sec_token <- news_text %>%
     include_docvars = TRUE,
     padding = TRUE,
     verbose = quanteda_options("verbose")
-  ) %>% 
-  tokens_tolower() %>% 
+  ) |>
+  tokens_tolower() |>
   tokens_wordstem(
     language = "english"
   )
 
-sec_dfm <- sec_token %>% 
+sec_dfm <- sec_token |>
   dfm(
     remove_numbers = FALSE, 
     remove_punct = TRUE, 
     remove_symbols = TRUE, 
     remove = stopwords("english")
-  ) %>% 
+  ) |>
   dfm_trim(
     # min_docfreq = 0.05, 
     # max_docfreq = 0.90, 
     docfreq_type = "prop"
   )
 
-topic_count <- 10
+topic_count <- 2
 
-sec_stm <- sec_dfm %>% 
+sec_stm <- sec_dfm |>
   convert(
     to = "stm"
-  ) 
-  
-stm_model_sec <- stm(documents = sec_stm$documents,
-    vocab = sec_stm$vocab,
-    K = topic_count,
-    data = sec_stm$meta,
-    init.type = "Random")
+  )
+
+stm_model_sec <- stm(
+  documents = sec_stm$documents,
+  vocab = sec_stm$vocab,
+  K = topic_count,
+  data = sec_stm$meta,
+  init.type = "Random")
+
+stm_model_sec$version
 
 # plot(stm_model_sec,
 #      type = "perspectives",
